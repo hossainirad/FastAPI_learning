ac211ca70ecb5f9df030ad4a625e858135bf63fc
ali <ali.hossaini34@yahoo.com>
2023-02-18 14:51:34 +0330

    add Path()

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, validator
 from validators import normalize_name, normalize_email
+from fastapi import Path
 
 
 class Person(BaseModel):
@@ -7,6 +8,7 @@ class Person(BaseModel):
 	name: str
 	username = 'Default Username'
 	email: str
+	age: int = Path(le=25, ge=10)
 
 	_normalize_name = validator('name', allow_reuse=True)(normalize_name)
 	_normalize_email = validator('email', allow_reuse=True)(normalize_email)
