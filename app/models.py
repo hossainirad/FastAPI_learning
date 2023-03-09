2b32c35f265f7c930d861cdca5c5a8ed6aff9ed9
ali <ali.hossaini34@yahoo.com>
2023-03-10 02:25:20 +0330

    add username field to User model

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
